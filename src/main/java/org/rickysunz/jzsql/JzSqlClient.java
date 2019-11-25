8efe8f0b5902aee341d1f4347875cd203f7b333d
rickysunnz <ricky.x.sun@hotmail.com>
2019-11-25 14:26:59 +1300

    Add basic interface for JzSqlClient

--- src/main/java/org/rickysunz/jzsql/JzSqlClient.java
+++ src/main/java/org/rickysunz/jzsql/JzSqlClient.java
@@ -1,9 +1,42 @@
 package org.rickysunz.jzsql;
 
+import java.sql.Connection;
+import java.util.List;
+
 public class JzSqlClient {
+	
+	private Connection connection;
 
 	public JzSqlClient() {
 		
+	}
+	
+	public JzSqlClient(Connection connection) {
+		this.connection = connection;
+	}
+	
+	public int insert(Object item) {
+		return 0;
+	}
+	
+	public int update(Object item) {
+		return 0;
+	}
+	
+	public int execute(String sql) {
+		return 0;
+	}
+	
+	public int execute(String sql,Object ... param) {
+		return 0;
+	}
+	
+	public List<Object> query(String sql) {
+		return null;
+	}
+	
+	public List<Object> query(String sql, Object ... param) {
+		return null;
 	}
 
 }
